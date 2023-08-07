8ba3743e44e3023c10b671726ca95f04f08a9ab4
Eduardo Dantas Luna <michaelthefear@gmail.com>
2023-08-06 23:02:06 -0300

    Deploy test 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import CAH from './components/cah/Page.jsx'
 import CAHCardCreator from './components/cah/CardCreator.jsx'
+import Trunfo from './components/trunfo/Page.jsx'
 
 import {
   createBrowserRouter,
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
     { path: "/", element: <App /> },
     { path: "/cah", element: <CAH /> },
     { path: "/cah/card-creator", element: <CAHCardCreator /> },
+    { path: "/trunfo", element: <Trunfo /> },
   ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
