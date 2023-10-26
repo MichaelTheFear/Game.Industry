f2141b6b9343e8d0bf96e6d9cdf9e7532eccfb52
Eduardo Dantas Luna <michaelthefear@gmail.com>
2023-10-26 17:23:43 -0300

    Sync

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Home from './App.jsx'
 import CAH from './components/cah/Page.jsx'
 import CAHCardCreator from './components/cah/CardCreator.jsx'
 import Trunfo from './components/trunfo/Page.jsx'
@@ -12,15 +12,16 @@ import {
 import './index.css'
 
 const router = createBrowserRouter([
-    { path: "/", element: <App /> },
+    { path: "/", element: <Home /> },
     { path: "/cah", element: <CAH /> },
     { path: "/cah/card-creator", element: <CAHCardCreator /> },
     { path: "/trunfo", element: <Trunfo /> },
-  ],{basename: import.meta.env.DEV ? '' : '/Game.Industry'});
+  ],{basename: import.meta.env.DEV ? '/' : '/Game.Industry/'});
 
+  
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  import.meta.env.DEV ? <React.StrictMode>
+  <RouterProvider router={router} />
+</React.StrictMode> : <RouterProvider router={router} />,
 
 )
